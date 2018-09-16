a873ebb3788f40ad027a812497c4805d542ff1f5
jcarraca <jcarraca@deloitte.pt>
2018-09-16 17:09:39 -0500

    .

--- www/dpplugin.js
+++ www/dpplugin.js
@@ -1,5 +1,5 @@
 // Empty constructor
-function EntelPlugin() {}
+function DPPlugin() {}
 
 // The function that passes work along to native shells
 // Message is a string, duration may be 'long' or 'short'
